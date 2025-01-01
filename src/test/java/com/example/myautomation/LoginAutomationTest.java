c4b358847c5dd06debe70e090b3cb8c56e75fa3a
Kavya013 <115550941+Kavya013@users.noreply.github.com>
2025-01-01 19:23:13 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/myautomation/LoginAutomationTest.java
+++ src/test/java/com/example/myautomation/LoginAutomationTest.java
@@ -1,10 +1,8 @@
 package com.example.myautomation;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class LoginAutomationTest {
     @Test
     public void testLogin() {
@@ -12,16 +10,10 @@ public class LoginAutomationTest {
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://www.saucedemo.com/");
-            
-            // Create an instance of LoginPage
             LoginPage loginPage = new LoginPage(driver);
-            
-            // Perform login actions
             loginPage.enterUsername("standard_user");
             loginPage.enterPassword("secret_sauce");
             loginPage.clickLogin();
-            
-            // Validate the page title after login
             String expectedTitle = "Swag Labs";
             String actualTitle = loginPage.getPageTitle();
             assertEquals(expectedTitle, actualTitle);
