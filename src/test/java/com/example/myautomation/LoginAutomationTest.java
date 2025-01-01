da72e4f0b41c94b1cec7feee7390145bff980bcf
Kavya013 <115550941+Kavya013@users.noreply.github.com>
2025-01-01 16:38:10 +0530

    Jenkins

--- src/test/java/com/example/myautomation/LoginAutomationTest.java
+++ src/test/java/com/example/myautomation/LoginAutomationTest.java
@@ -11,14 +11,14 @@ public class LoginAutomationTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\prabh\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         try {
-            driver.get("https://the-internet.herokuapp.com/login");
+            driver.get("https://www.saucedemo.com/");
             WebElement usernameField = driver.findElement(By.id("username"));
             WebElement passwordField = driver.findElement(By.id("password"));
-            WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
-            usernameField.sendKeys("testUser");
-            passwordField.sendKeys("testPassword");
+            WebElement loginButton = driver.findElement(By.id("loginButton"));
+            usernameField.sendKeys("standard_user");
+            passwordField.sendKeys("secret_sauce");
             loginButton.click();
-            String expectedTitle = "Dashboard";
+            String expectedTitle = "Swag Labs";
             String actualTitle = driver.getTitle();
             assertEquals(expectedTitle, actualTitle);
         } finally {
