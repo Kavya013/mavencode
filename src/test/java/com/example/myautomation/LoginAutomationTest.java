42843415e2bb87c9a5c1bf0d713920c4cb310931
Kavya013 <115550941+Kavya013@users.noreply.github.com>
2025-01-01 16:30:06 +0530

    Jenkins

--- src/test/java/com/example/myautomation/LoginAutomationTest.java
+++ src/test/java/com/example/myautomation/LoginAutomationTest.java
@@ -11,10 +11,10 @@ public class LoginAutomationTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\prabh\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         try {
-            driver.get("https://example.com/login");
+            driver.get("https://the-internet.herokuapp.com/login");
             WebElement usernameField = driver.findElement(By.id("username"));
             WebElement passwordField = driver.findElement(By.id("password"));
-            WebElement loginButton = driver.findElement(By.id("loginButton"));
+            WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
             usernameField.sendKeys("testUser");
             passwordField.sendKeys("testPassword");
             loginButton.click();
